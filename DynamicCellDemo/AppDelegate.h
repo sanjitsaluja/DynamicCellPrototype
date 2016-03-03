ee2640c1d9e3d9f372b73297db70efcde85f8632
Sanjit Saluja <sanjits@zillow.com>
2016-03-03 11:59:54 +0530

    Initial Commit

---
+++ DynamicCellDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicCellDemo
+//
+//  Created by Sanjit Saluja on 3/3/16.
+//  Copyright © 2016 Sanjit Saluja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DynamicCellDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicCellDemo
+//
+//  Created by Sanjit Saluja on 3/3/16.
+//  Copyright © 2016 Sanjit Saluja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
